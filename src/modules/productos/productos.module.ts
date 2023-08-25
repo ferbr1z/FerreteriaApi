6aaea250e38a8699c06e9d1afdace6bac11f85a5
Fernando Brizuela <ffba97@gmail.com>
2023-08-25 18:34:19 -0400

    (fix) modulo de productos corregido

--- src/modules/productos/productos.module.ts
+++ src/modules/productos/productos.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 
 import {TypeOrmModule} from '@nestjs/typeorm'
 import { Producto } from './entity/producto.entity';
+import { ProductosController } from 'src/controllers/productos/productos.controller';
+import { ProductosService } from 'src/services/productos/productos.service';
 @Module({
     imports:[TypeOrmModule.forFeature([Producto])],
+    controllers:[ProductosController],
+    providers:[ProductosService],
     exports: [TypeOrmModule.forFeature([Producto])]
 })
 export class ProductosModule {}
