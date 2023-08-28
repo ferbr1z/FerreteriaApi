3dfd63ebb610e3b62378e212299aeb7d6b896904
Fernando Brizuela <ffba97@gmail.com>
2023-08-28 12:52:27 -0400

    (+) categorias

--- src/services/categorias/categorias.service.ts
+++ src/services/categorias/categorias.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Categoria } from 'src/modules/categorias/entity/categoria.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CategoriasService {}
+export class CategoriasService {
+    constructor(@InjectRepository(Categoria) categoriaRepo: Repository<Categoria>){}
+
+    async create(createCategoria ){
+        
+    }
+
+}
